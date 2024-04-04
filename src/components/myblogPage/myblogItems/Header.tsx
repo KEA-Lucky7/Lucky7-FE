f2410df8e29f1f816659c8c2964106c12c899902
jodan <wdd789q@naver.com>
2024-04-04 20:19:51 +0900

    feat : Header 아이콘 추가, 기존 뼈대 작업

--- src/components/myblogPage/myblogItems/Header.tsx
+++ src/components/myblogPage/myblogItems/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import * as S from "../styles/HeaderStyled.ts";
+import search from "../../../assets/header/search.png";
+import notice from "../../../assets/header/notice.png";
+import setting from "../../../assets/header/setting.png";
+import logout from "../../../assets/header/logout.png";
 export default function Header() {
   return (
     <S.Header>
-        헤더자리입니다.
+        <S.LogoBox>로고자리</S.LogoBox>
+        <S.IconBox>
+          <img src={search} alt='search' width={'40px'} height={'40px'}/>
+          <img src={notice} alt='search' width={'50px'} height={'40px'}/>
+          <img src={setting} alt='search' width={'40px'} height={'40px'}/>
+          <img src={logout} alt='search' width={'40px'} height={'40px'}/>
+        </S.IconBox>
     </S.Header>
   )
 }
--- src/components/myblogPage/styles/HeaderStyled.ts
+++ src/components/myblogPage/styles/HeaderStyled.ts
@@ -4,6 +4,27 @@ export const Header = styled.div`
     width: 100%;
     height: 100px;
     border: 1px solid red;
+    display: flex;
+    justify-content: space-between;
 `;
+
+export const LogoBox = styled.div`
+    width: 10%;
+    height: 40px;
+    border: 1px solid red;
+    margin: 20px;
+`;
+
+
+export const IconBox = styled.div`
+    width: 20%;
+    height: 40px;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    margin-top: 20px;
+`;
+
 
 
