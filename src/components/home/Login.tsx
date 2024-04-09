a61173400127fef51f9d1b8b803f01b336589420
jrary <jrary@naver.com>
2024-04-09 15:50:27 +0900

    feat: 구글 소셜로그인 방식 변경

--- src/components/home/Login.tsx
+++ src/components/home/Login.tsx
@@ -1,44 +1,17 @@
 import Cookies from "js-cookie";
-import { GoogleOAuthProvider, GoogleLogin, CredentialResponse } from "@react-oauth/google";
 const { Kakao } = window;
 import { jwtDecode } from "jwt-decode";
-
-// 구글 로그인 시 리턴값
-// interface response {
-//     clientId: string;
-//     credential: string;
-//     select_by: string;
-// }
-
-// Google Credential jwt decode시 저장되어 있는 정보
-interface googleCredential {
-    aud: string;
-    azp: string;
-    email: string;
-    email_verified: boolean;
-    exp: number;
-    family_name: string;
-    given_name: string;
-    iat: number;
-    iss: string;
-    jti: string;
-    name: string;
-    nbf: number;
-    picture: string;
-    sub: string;
-}
 
 const Login = () => {
     const googleId = import.meta.env.VITE_GOOGLE_ID;
     const googleSecret = import.meta.env.VITE_GOOGLE_SECRET;
     const kakaoKey = import.meta.env.VITE_KAKAO_KEY;
     // 구글 소셜로그인
-    const handleGoogleLogin = (credentialResponse) => {
-        console.log(credentialResponse);
-        const responsePayload: googleCredential = jwtDecode(credentialResponse.credential);
-        // (임시 확인용) 정보 출력
-        console.log("ID: " + responsePayload.sub);
-        console.log('Name: ' + responsePayload.name);
+    const handleGoogleLogin = () => {
+
+        const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://localhost:5173&response_type=code&client_id=${googleId}&scope=https://www.googleapis.com/auth/userinfo.profile`;
+        window.location.href = GOOGLE_AUTH_URL;
+
         // 테스트 완료 시 주석 해제
         // Cookies.set('member', 'true', { expires: 1 });
         // 나중에 useEffect로 디테일 설정해야함
@@ -62,17 +35,9 @@ const Login = () => {
 
     return (
         <div>
-            <GoogleOAuthProvider clientId={googleId}>
-                <GoogleLogin
-                    onSuccess={(credentialResponse) => {
-                        handleGoogleLogin(credentialResponse);
-                        console.log("로그인 성공")
-                      }}
-                    onError={() => {
-                        console.error("로그인 실패");
-                      }}
-                />
-            </GoogleOAuthProvider>
+            <button onClick={handleGoogleLogin} >
+                Google로 로그인
+            </button>
             <button onClick={handleKakaoLogin} >
                 Kakao로 로그인
             </button>
