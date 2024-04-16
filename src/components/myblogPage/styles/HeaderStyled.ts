f5cf66bfbde88a2e92413854f49e6389c860b038
jodan <wdd789q@naver.com>
2024-04-17 02:22:50 +0900

    feat : 디자인 된 헤더로 View 변경 작업

--- src/components/myblogPage/styles/HeaderStyled.ts
+++ src/components/myblogPage/styles/HeaderStyled.ts
@@ -9,22 +9,57 @@ export const Header = styled.div`
 `;
 
 export const LogoBox = styled.div`
-    width: 10%;
+    width: 180px;
     height: 40px;
     border: 1px solid red;
-    margin: 20px;
+    margin: 20px 20px 20px 80px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
 `;
 
 
 export const IconBox = styled.div`
-    width: 20%;
+    width: 43%;
     height: 40px;
     border: 1px solid red;
     display: flex;
     flex-direction: row;
-    gap: 10px;
     margin-top: 20px;
+    gap: 30px;
 `;
 
 
+export const SearchContainer = styled.div`
+  width: 330px;
+  height: 35px;
+  border-radius: 15px;
+  border: solid 2px black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  opacity: 1;
+`;
 
+export const Img = styled.img`
+  border: none;
+  text-align: center;
+  margin-right: 10px;
+  overflow: auto;
+  z-index: -1;
+  font-size: 15px;
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+  border: none;
+  outline: none;
+  text-align: center;
+  margin-left: 10px;
+  overflow: auto;
+  z-index: -1;
+  font-size: 15px;
+
+`;
