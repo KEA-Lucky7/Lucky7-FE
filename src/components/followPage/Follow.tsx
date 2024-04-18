d920a215c57eeecdcf772b6187e000401b53d349
jrary <jrary@naver.com>
2024-04-18 22:14:59 +0900

    feat: Follow 화면 리스트 재활용

--- src/components/followPage/Follow.tsx
+++ src/components/followPage/Follow.tsx
@@ -1,31 +1,22 @@
 import { useEffect, useState } from "react";
-import MainTitle from "./FollowTitle";
-import HomePostList from "./FollowPostList";
 import Header from "../myblogPage/myblogItems/Header";
-// import * as S from "../homePage/styles/MainStyle";
-import * as S from "../myblogPage/styles/MyblogPostCss";
-import Titlebackground from "../../assets/myblog/Titlebackground.png";
-import Editbutton from "../../assets/myblog/Editbutton.png";
-import MyblogWidget from '../myblogPage/MyblogWidget';
-import MyblogCategoryWidget from '../myblogPage/MyblogCategoryWidget'; 
-import MyblogPostList from "../myblogPage/MyblogPostList";
+import * as MS from "../homePage/styles/MainStyle";
+import * as BS from "../myblogPage/styles/MyblogPostCss";
+import FollowPostList from "../followPage/MyblogPostList";
 
 const Follow = () => {
 
     return (
-        // <S.HomeContainer>
-        //     <Header />
-        //     <S.MainContainer>
-        //         <MainTitle />
-        //         <div>BEST</div>
-        //         <HomePostList />
-        //     </S.MainContainer>
-        // </S.HomeContainer>
-        <S.PostContainer>
-        <S.RightSection>
-          <MyblogPostList />
-        </S.RightSection>
-      </S.PostContainer>
+        <MS.HomeContainer>
+            <Header />
+            <MS.MainContainer>
+                <div>내 팔로우</div>
+                <BS.RightSection>
+                    <FollowPostList />
+                </BS.RightSection>
+            </MS.MainContainer>
+        </MS.HomeContainer>
+
     );
 }
 
