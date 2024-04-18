ee2d57f2dfc7fd78014a7ae847be6508baa0132e
jrary <jrary@naver.com>
2024-04-18 17:44:37 +0900

    feat: 홈 화면 게시물 hover 이벤트 만들기

--- src/components/homePage/HomePostList.tsx
+++ src/components/homePage/HomePostList.tsx
@@ -12,20 +12,7 @@ const HomePostList = () => {
         setVisibleItems(prevVisibleItems => prevVisibleItems + 6);
     };
 
-    // 각 행에서 가장 긴 제목의 길이를 찾는 함수
-    const findLongestTitleLength = (startIndex, endIndex) => {
-        let maxLength = 0;
-        for (let i = startIndex; i < endIndex; i++) {
-            const post = bestContents[i];
-            if (post.title.length > maxLength) {
-                maxLength = post.title.length;
-            }
-        }
-        return maxLength;
-    };
-
-    // 가장 긴 제목의 길이를 가진 아이템의 인덱스를 찾는 함수
-    const findLongestTitleIndex = (startIndex, endIndex) => {
+    const findLongestTitleIndex = (startIndex: number, endIndex: number) => {
         let maxLength = 0;
         let longestIndex = -1;
         for (let i = startIndex; i < endIndex; i++) {
@@ -40,26 +27,28 @@ const HomePostList = () => {
 
     return (
         <div>
-            {/* 열을 4개로 나누고 각 행에서 가장 긴 제목의 길이를 찾아 해당 아이템의 가로 길이를 2배로 설정 */}
-            <S.ListContainer style={{ gridTemplateColumns: "repeat(4, 1fr)" }}>
+            <S.ListContainer style={{ gridTemplateColumns: "repeat(7, 1fr)" }}>
                 {bestContents.slice(0, visibleItems).map((post, index) => {
                     const rowStartIndex = Math.floor(index / 3) * 3;
                     const rowEndIndex = rowStartIndex + 3;
                     const longestTitleIndex = findLongestTitleIndex(rowStartIndex, rowEndIndex);
+                    const gridColumn = index === longestTitleIndex ? "span 3" : "span 2";
                     return (
                         <S.ListItemContainer 
                             key={post.id}
-                            // 가장 긴 제목의 아이템에 대해 가로 길이를 2배로 설정
-                            style={{ gridColumn: index === longestTitleIndex ? "span 2" : "span 1" }}
+                            style={{ gridColumn }}
                         >                    
                             <S.ListItemBox
                                 onMouseEnter={() => setHoveredItemId(post.id)}
                                 onMouseLeave={() => setHoveredItemId(0)}
+                                style={{ backgroundImage: hoverItem === post.id ? `url(${post.thumbnail})` : 'none' }}
                             >
-                                <div>{post.title}</div>
-                                {hoverItem === post.id && (
-                                    <div>{post.content}</div>
-                                )}
+                                <S.ListItemBoxContents>
+                                    <div>{post.title}</div>
+                                    {hoverItem === post.id && (
+                                        <div>{post.content}</div>
+                                    )}
+                                </S.ListItemBoxContents>
                             </S.ListItemBox>
                             <div>{post.writer}님의 일기</div>
                             <div>#{post.tag}</div>
@@ -69,7 +58,7 @@ const HomePostList = () => {
                     );
                 })}
             </S.ListContainer>
-            {visibleItems <= 18 ? (
+            {visibleItems <= 30 ? (
                 <button onClick={handleShowMore}>더보기</button>
             ) : (
                 <div>인기글 목록을 전부 불러왔습니다.</div>
--- src/components/homePage/styles/HomePostListStyle.ts
+++ src/components/homePage/styles/HomePostListStyle.ts
@@ -10,11 +10,26 @@ export const ListContainer = styled.div`
 export const ListItemBox = styled.div`
     height: 270px;
     border: 1px solid #ccc;
-    background-color: #DEDEDE;
+    background-color: #dedede;
     padding: 20px;
     &:hover {
-        background-color: #777777;
+        // filter: brightness(0.7);
     }
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    display: flex;
+    justify-contents: center:
+    align-items: center;
+`;
+
+export const ListItemBoxContents = styled.div`
+    border: 1px solid #ccc;
+    margin: auto;
+    color: #ffffff;
+    filter: brightness(1.95);
+    text-align: center;
+    width: 80%;
 `;
 
 export const ListItemContainer = styled.div`
