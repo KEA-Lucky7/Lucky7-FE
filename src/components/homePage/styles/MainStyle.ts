e69dbd03e5a6e28af5be924a4a851b97f296c25a
HOAN_C <hoan.c9907@gmail.com>
2024-04-20 23:17:06 +0900

    Add style on MainTitle

--- src/components/homePage/styles/MainStyle.ts
+++ src/components/homePage/styles/MainStyle.ts
@@ -1,21 +1,15 @@
 import styled from "styled-components";
 
 export const HomeContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border: 1px solid #00ff00;
-    height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid red;
+  width: 100%;
 `;
 
 export const MainContainer = styled.div`
-    width: calc(100% - 700px);
-    margin: 20px 300px;
-    border: 1px solid #0000ff;
-`;
-
-export const MainTitleContainer = styled.div`
-    width: 100%;
-    height: 350px;
-    border: 1px solid #003300;
+  border: 1px solid blue;
+  width: 70%;
+  margin: 20px 300px;
 `;
---
+++ src/components/homePage/styles/MainTitle.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const MainTitleContainer = styled.div`
+  border: 1px solid red;
+  width: 100%;
+  height: 350px;
+  border-radius: 5px;
+  background-color: #d4d4d4;
+  color: #ffffff;
+  text-align: right;
+  position: relative;
+`;
+
+export const TextContainer = styled.div`
+  border: 1px solid red;
+  font-size: 25px;
+  position: absolute;
+  right: 30px; /* 우측 여백 */
+  bottom: 30px; /* 하단 여백 */
+`;
+
+export const Text = styled.div`
+  margin: 3px;
+`;
+
+export const ByMoaBoa = styled.div`
+  font-size: 16px;
+`;
