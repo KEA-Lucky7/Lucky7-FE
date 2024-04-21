8ffb503f5f6965ea6a7386dbd6d5e7066302a793
jodan <wdd789q@naver.com>
2024-04-22 03:06:57 +0900

    feat : 게시글 상세보기 본문 & 태그 View 구현

--- src/components/postDetailPage/PostDetail.tsx
+++ src/components/postDetailPage/PostDetail.tsx
@@ -3,6 +3,8 @@ import * as S from "./styles/PostDetailCss";
 import postDetailbackground from "../../assets/postDetail/postDetailbackground.png";
 import seeMore from "../../assets/postDetail/seeMore.png";
 import report from "../../assets/postDetail/report.png";
+import postPicture from "../../assets/postDetail/postPicture.png";
+import { postContentData } from "../../data/userBlog/postContentData";
 
 export default function PostDetail() {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
@@ -42,20 +44,47 @@ export default function PostDetail() {
         </S.Picturecontainer>
       </S.Container>
 
-      {/* 본문 맨 위 내용 */}
-      <S.PostBox>
-        <div onClick={handleCopyURL}>URL 복사</div>
-        <img src={seeMore} alt='더보기' style={{ width: '3px', height: '19px' }} onClick={toggleDropdown} />
-        {/* 드롭다운 메뉴 내용 */}
-        {isDropdownVisible && (
-          <div style={{ position: 'absolute', top: '520px', right: '285px', backgroundColor: '#fff', border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }}>
-            <div style={{ display: 'flex', flexDirection: 'row', gap: '10px', justifyContent: 'center', alignItems: 'center' }}>
-              신고하기
-              <img src={report} alt='신고' style={{ width: '24px', height: '24px' }} />
+
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {/* 본문 맨 위 내용 */}
+        <S.PostIntro>
+          <div onClick={handleCopyURL}>URL 복사</div>
+          <img src={seeMore} alt='더보기' style={{ width: '3px', height: '19px' }} onClick={toggleDropdown} />
+          {/* 드롭다운 메뉴 내용 */}
+          {isDropdownVisible && (
+            <div style={{ position: 'absolute', top: '520px', right: '285px', backgroundColor: '#fff', border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }}>
+              <div style={{ display: 'flex', flexDirection: 'row', gap: '10px', justifyContent: 'center', alignItems: 'center' }}>
+                신고하기
+                <img src={report} alt='신고' style={{ width: '24px', height: '24px' }} />
+              </div>
             </div>
+          )}
+        </S.PostIntro>
+        {/* 본문 내용 */}
+        <S.PostBox>
+          <img src={postPicture} alt='본문사진' style={{ width: '100%', height: '600px' }} />
+          {postContentData.map((line, index) => (
+            <div key={index}>{line}</div>
+          ))}
+        </S.PostBox>
+
+        {/* 태그 내용 */}
+        <S.TagBox>
+          <S.FirstTag>#대표태그</S.FirstTag>
+          <div style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
+            <S.SecondTag>
+              #서브태그
+            </S.SecondTag>
+            <S.SecondTag>
+              #서브태그
+            </S.SecondTag>
+            <S.SecondTag>
+              #서브태그
+            </S.SecondTag>
           </div>
-        )}
-      </S.PostBox>
+        </S.TagBox>
+      </div>
+
     </>
   )
 }
--- src/components/postDetailPage/styles/PostDetailCss.ts
+++ src/components/postDetailPage/styles/PostDetailCss.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 
 interface PictureContainerProps {
     imageUrl: string;
-  }
+}
 
-  
+
 export const Container = styled.div`
     width: 100%;
     height: 100%;
@@ -18,20 +18,15 @@ export const Container = styled.div`
 
 
 export const Picturecontainer = styled.div<PictureContainerProps>`
-  width: 70%;
-  height: 400px;
-  margin-top: 10px;
-  position: relative;
-  background: ${({ imageUrl }) =>
-    `black url(${imageUrl}) no-repeat center center`};
-  background-size: cover;
-  border-radius: 4px;
-  margin: 0 auto;
-`;
-
-export const PostDetailBackgroundImage = styled.div`
-    width: 100%;
+    width: 70%;
+    height: 400px;
+    margin-top: 10px;   
     position: relative;
+    background: ${({ imageUrl }) =>
+        `black url(${imageUrl}) no-repeat center center`};
+    background-size: cover;
+    border-radius: 4px;
+    margin: 0 auto;
 `;
 
 
@@ -42,10 +37,12 @@ export const TextBox = styled.div`
     border: 1px solid blue;
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 
 export const FirstLine = styled.div`
-    width: 100%;
+    width: 50%;
     border: 1px solid blue;
     display: flex;
     flex-direction: row;
@@ -53,26 +50,24 @@ export const FirstLine = styled.div`
     font-size: 24px;
     position: absolute;
     top: 210px;
-    left: 40px;
+    left: 380px;
     
 `;
 
 export const SecondLine = styled(FirstLine)`
     top: 250px;
-    left: 40px;
     font-size: 40px;
 
 `;
 
 export const ThirdLine = styled(FirstLine)`
     top: 350px;
-    left: 40px;
 `;
 
 
 
-export const PostBox = styled.div`
-    width: 70%;
+export const PostIntro = styled.div`
+    width: 30%;
     height: 100%;
     margin: 0 auto;
     border: 1px solid blue;
@@ -83,3 +78,59 @@ export const PostBox = styled.div`
     justify-content: right;
     padding-top: 10px;
 `;
+
+export const PostBox = styled(PostIntro)`
+    flex-direction: column;
+    text-align: left;
+    justify-content: left;
+`;
+
+
+export const TagBox = styled(PostIntro)`
+    display: flex;
+    flex-direction: column;
+    padding-top: 100px;
+    gap: 10px;
+
+`;
+
+
+
+
+export const ContentBox = styled.div`
+    width: 30%;
+    height: 100%;
+    margin: 0 auto;
+    border: 1px solid blue;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    text-align: left;
+    justify-content: left;
+    padding-top: 10px;
+`;
+
+export const FirstTag = styled.div`
+    width: 20%;
+    height: 30px;
+    border-radius: 10px;
+    background-color: #eaeaea;
+    text-align: left;
+    font-size: 18px;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #49519e;
+`;
+
+export const SecondTag = styled(FirstTag)`
+    width: 15%;
+    height: 30px;
+    font-size: 14px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+
+`;
+
+
