8a500e1194689ec22badc762f54c685fdd135511
jrary <jrary@naver.com>
2024-04-23 21:33:10 +0900

    feat: 그래프 디자인

--- src/components/reportPage/ReportMonthly.tsx
+++ src/components/reportPage/ReportMonthly.tsx
@@ -3,87 +3,129 @@ import { Bar } from "react-chartjs-2";
 import { Chart, registerables } from 'chart.js';
 import data from "../../data/report/MonthlyAmountSpent.json";
 import * as S from "./styles/ReportMonthlyStyle";
+import leftBtn from "../../assets/button/left.png";
+import rightBtn from "../../assets/button/right.png";
 
 Chart.register(...registerables);
 
 const ReportMonthly = () => {
-  const [chartData, setChartData] = useState<{
-    labels: string[];
-    datasets: {
-      label: string;
-      data: number[];
-      backgroundColor: string;
-    }[];
-  }>({
-    labels: [],
-    datasets: [
-      {
-        label: 'Monthly Expenses',
-        data: [],
-        backgroundColor: '#aaaaaa'
-      }
-    ]
-  });
+    // 현재 연도 설정
+    const [currentYear, setCurrentYear] = useState<number>(2024);
+    // 차트에 사용될 데이터 useState 정의
+    const [chartData, setChartData] = useState<{
+        labels: string[];
+        datasets: {
+        label: string;
+        data: number[];
+        backgroundColor: string;
+        }[];
+    }>({
+        labels: [],
+        datasets: [
+        {
+            label: '월간 지출',
+            data: [],
+            backgroundColor: '#ff8989'
+        }
+        ]
+    });
+    // 현재 연도의 데이터 불러오기
+    useEffect(() => {
+        const spentData: number[] = [];
+        const labels: string[] = [];
+        setCurrentYear(new Date().getFullYear())
+        // 데이터 가져오기
+        for (let i = 1; i <= 12; i++) {
+        const month = `${currentYear}-${i.toString().padStart(2, "0")}`;
+        labels.push(month);
+        const dataForMonth = data.find((item) => item.date === month);
+        spentData.push(dataForMonth ? dataForMonth.spent : 0);
+        }
+        // 가져온 데이터 useState로 저장
+        setChartData((prevChartData) => ({
+        ...prevChartData,
+        labels,
+        datasets: [{
+            ...prevChartData.datasets[0],
+            data: spentData
+        }]
+        }));
+    }, []);
+    // 그래프 x축 레이블
+    const monthLabels = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"];
+    // 연도 총 소비 액수 계산
+    const totalSpent = chartData.datasets.reduce((total, dataset) => {
+        return total + dataset.data.reduce((sum, value) => sum + value, 0);
+    }, 0);
 
-  useEffect(() => {
-    const labels: string[] = [];
-    const spentData: number[] = [];
-    const currentYear = new Date().getFullYear();
-
-    for (let i = 1; i <= 12; i++) {
-      const month = `${currentYear}-${i.toString().padStart(2, "0")}`;
-      labels.push(month);
-      const dataForMonth = data.find((item) => item.date === month);
-      spentData.push(dataForMonth ? dataForMonth.spent : 0);
-    }
-
-    setChartData((prevChartData) => ({
-      ...prevChartData,
-      labels,
-      datasets: [{
-        ...prevChartData.datasets[0],
-        data: spentData
-      }]
-    }));
-  }, []);
-   
-  return (
-    <S.ReportMonthlyContainer>
-      <Bar
-        data={chartData}
-        options={{
-          scales: {
-            x: {
-              title: {
-                display: false,
-                text: "Month",
-              },
-              grid: {
-                display: false,
-              },
-              ticks: {
-                callback: (value, index, values) => {
-                    const customLabels = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"];
-                    return customLabels[index];
-                  }
-              }
+    return (
+        <div>
+        <S.TitleContainer>
+            <S.TitleYear>{currentYear}</S.TitleYear>
+            <S.TitleYearBtn src={leftBtn} />
+            <S.TitleYearBtn src={rightBtn} />
+        </S.TitleContainer>
+        <S.ReportMonthlyContainer>
+        <Bar
+            data={chartData}
+            options={{
+            scales: {
+                x: {
+                title: {
+                    display: false,
+                    text: "Month",
+                },
+                grid: {
+                    display: false,
+                },
+                ticks: {
+                    callback: (value, index, values) => {
+                        return monthLabels[index];
+                    }
+                }
+                },
+                y: {
+                title: {
+                    display: false,
+                    text: "Amount Spent",
+                },
+                },
             },
-            y: {
-              title: {
+            plugins: {
+                legend: {
                 display: false,
-                text: "Amount Spent",
-              },
+                },
             },
-          },
-          plugins: {
-            legend: {
-              display: false,
-            },
-          },
-        }}
-      />
-    </S.ReportMonthlyContainer>
-  );
+            }}
+        />
+        </S.ReportMonthlyContainer>
+        <S.TableContainer>
+            <table style={{ width: "100%" , border: "1px solid #ddd" }}>
+            <thead>
+                <tr>
+                <th>전체</th>
+                {chartData.labels.map((label, index) => (
+                    <th key={index} style={{ width: `${100 / 13}%` , border: "1px solid #ddd" }}>{monthLabels[index]}</th>
+                ))}
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>지출</td>
+                {chartData.datasets[0].data.map((data, index) => (
+                    <td key={index} style={{ width: `${100 / 13}%` , border: "1px solid #ddd" }}>{data}</td>
+                ))}
+                </tr>
+            </tbody>
+            </table>
+        </S.TableContainer>
+        <S.YearlyContainer>
+        {currentYear}년 총 지출액
+        {totalSpent}
+        원
+        </S.YearlyContainer>
+        </div>
+    );
 };
 
 export default ReportMonthly;
--- src/components/reportPage/styles/ReportMonthlyStyle.ts
+++ src/components/reportPage/styles/ReportMonthlyStyle.ts
@@ -1,9 +1,30 @@
 import styled from "styled-components";
 
+export const TitleContainer = styled.div`
+    display: flex;
+`;
+
+export const TitleYear = styled.div`
+    font-size: 18px;
+    margin: 0px 10px 0px 0px;
+`;
+
+export const TitleYearBtn = styled.img`
+    height: 13px;
+    cursor: pointer;
+    padding: 3px 3px 3px 3px;
+    margin: 1px 5px 1px 5px;
+`;
+
 export const ReportMonthlyContainer = styled.div`
   width: 100%;
-  height: 500px;
+  padding: 30px 0px 20px 0px;
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+export const TableContainer = styled.div``;
+
+export const YearlyContainer = styled.div`
 `;
