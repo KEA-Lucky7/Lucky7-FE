bf3ab26718446b2c654e7beb89d4f9972bd23516
HOAN_C <hoan.c9907@gmail.com>
2024-04-24 22:12:48 +0900

    가계부 더미 데이터 추가

--- src/components/myblogPage/AccountBook.tsx
+++ src/components/myblogPage/AccountBook.tsx
@@ -6,7 +6,38 @@ interface ExpenseData {
   amount: number;
 }
 
-const expenses: ExpenseData[] = [{ date: new Date(2024, 3, 7), amount: 33200 }];
+const expenses: ExpenseData[] = [
+  { date: new Date(2024, 3, 2), amount: 48370 },
+  { date: new Date(2024, 3, 3), amount: 29470 },
+  { date: new Date(2024, 3, 4), amount: 28320 },
+  { date: new Date(2024, 3, 6), amount: 21200 },
+  { date: new Date(2024, 3, 7), amount: 33200 },
+  { date: new Date(2024, 3, 8), amount: 47292 },
+  { date: new Date(2024, 3, 13), amount: 95254 },
+  // { date: new Date(2024, 3, 18), amount: 48030 },
+  { date: new Date(2024, 3, 27), amount: 59744 },
+  { date: new Date(2024, 3, 1), amount: 99774 },
+  { date: new Date(2024, 3, 5), amount: 72148 },
+  { date: new Date(2024, 3, 9), amount: 16851 },
+  { date: new Date(2024, 3, 10), amount: 16372 },
+  // { date: new Date(2024, 3, 11), amount: 52032 },
+  { date: new Date(2024, 3, 12), amount: 22589 },
+  { date: new Date(2024, 3, 14), amount: 50275 },
+  { date: new Date(2024, 3, 15), amount: 40130 },
+  // { date: new Date(2024, 3, 16), amount: 16699 },
+  { date: new Date(2024, 3, 17), amount: 19009 },
+  { date: new Date(2024, 3, 19), amount: 99351 },
+  { date: new Date(2024, 3, 20), amount: 61475 },
+  { date: new Date(2024, 3, 21), amount: 18196 },
+  { date: new Date(2024, 3, 22), amount: 49230 },
+  { date: new Date(2024, 3, 23), amount: 58703 },
+  // { date: new Date(2024, 3, 24), amount: 19810 },
+  { date: new Date(2024, 3, 25), amount: 76217 },
+  { date: new Date(2024, 3, 26), amount: 65435 },
+  { date: new Date(2024, 3, 28), amount: 57377 },
+  { date: new Date(2024, 3, 29), amount: 44889 },
+  { date: new Date(2024, 3, 30), amount: 15428 },
+];
 
 export default function AccountBook() {
   const [type, setType] = useState("calendar");
