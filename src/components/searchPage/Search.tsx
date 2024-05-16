88903f1822f6406c9041a4dd1fe7fa11682a2b9f
jrary <jrary@naver.com>
2024-05-16 11:17:33 +0900

    feat: 검색 기본화면

---
+++ src/components/searchPage/Search.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles/SearchStyle";
+import { useState } from "react";
+
+export default function Search() {
+
+  return (
+    <S.SearchContainer>
+      search
+    </S.SearchContainer>  
+  );
+}
---
+++ src/components/searchPage/styles/SearchStyle.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SearchContainer = styled.div`
+  width: 67vw;
+  height: 100%;
+  margin: 0 auto;
+  border: 1px solid red;
+`;
+
