648865b89fd1b4ee654e13565af8fe55df1124ad
jrary <jrary@naver.com>
2024-05-19 18:24:09 +0900

    feat: 검색 페이지 메뉴 설정 완료

--- src/components/searchPage/Search.tsx
+++ src/components/searchPage/Search.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import * as S from './styles/SearchStyle';
 import SearchBlog from './SearchBlog';
 import SearchPost from './SearchPost';
@@ -10,25 +11,43 @@ function getQuery() {
 }
 
 const Search: React.FC = () => {
-  const location = useLocation();
   const navigate = useNavigate();
   const query = getQuery();
   const keyword = query.get('q');
   const tab = query.get('tab') || 'post';
 
   const handleTabChange = (newTab: string) => {
-    query.set('tab', newTab);
+    if (newTab === 'post') {
+      query.delete('tab');
+    } else {
+      query.set('tab', newTab);
+    }
     navigate({ search: query.toString() });
   };
 
   return (
     <S.SearchContainer>
-      <div>{keyword}</div>
-      <nav>
-        <button onClick={() => handleTabChange('post')}>Post</button>
-        <button onClick={() => handleTabChange('blog')}>Blog</button>
-        <button onClick={() => handleTabChange('user')}>User</button>
-      </nav>
+      <S.SearchKeyword>{keyword}</S.SearchKeyword>
+      <S.SearchMenuContainer>
+        <S.SearchMenu
+          onClick={() => handleTabChange('post')}
+          selected={tab === 'post'}
+        >
+          글
+        </S.SearchMenu>
+        <S.SearchMenu
+          onClick={() => handleTabChange('blog')}
+          selected={tab === 'blog'}
+        >
+          블로그
+        </S.SearchMenu>
+        <S.SearchMenu
+          onClick={() => handleTabChange('user')}
+          selected={tab === 'user'}
+        >
+          별명 아이디
+        </S.SearchMenu>
+      </S.SearchMenuContainer>
       <div>
         {tab === 'post' && <SearchPost />}
         {tab === 'blog' && <SearchBlog />}
--- src/components/searchPage/styles/SearchStyle.ts
+++ src/components/searchPage/styles/SearchStyle.ts
@@ -4,6 +4,30 @@ export const SearchContainer = styled.div`
   width: 67vw;
   height: 100%;
   margin: 0 auto;
-  border: 1px solid red;
 `;
 
+export const SearchKeyword = styled.div`
+  margin: 60px 0px 0px 0px;
+  font-size: 27px;
+`;
+
+export const SearchMenuContainer = styled.div`
+  width: 100%;
+  display: flex;
+  margin: 30px 0px 0px 0px;
+  border-bottom: 1px solid #aaa
+`;
+
+export const SearchMenu = styled.div<{ selected: boolean }>`
+  font-size: 18px;
+  text-decoration: none;
+  padding: 10px;
+  width: 12vw;
+  color: ${({ selected }) => (selected ? "black" : "#aaa")};
+  border-bottom: 2px solid ${({ selected }) => (selected ? 'gray' : 'transparent')};
+
+  &:hover {
+    color: ${({ selected }) => (selected ? "black" : "gray")};
+    border-bottom: 2px solid ${({ selected }) => (selected ? 'gray' : '#aaa')};
+  }
+`;
