16491c248a3cb418cd51ad704c1379cd1b325cae
jrary <jrary@naver.com>
2024-05-21 11:04:27 +0900

    feat: 글 목록 화면

--- src/components/searchPage/SearchPost.tsx
+++ src/components/searchPage/SearchPost.tsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import postList from "../../data/search/searchPostList.json";
 
-import * as S from "./styles/SearchStyle";
+import * as S from "./styles/SearchPostStyle";
 
-export default function SearchPost () {
-
+export default function SearchPost() {
   return (
     <S.SearchContainer>
-      post
-    </S.SearchContainer>  
+      {postList.map((post, index) => (
+        <S.PostCard key={index}>
+        <S.PostContent>
+          <S.Title>{post.title}</S.Title>
+          <S.Content>{post.content}</S.Content>
+          <S.UserInfo>
+            <S.UserImage src={post.user_image} alt={post.user_name} />
+            <S.UserName>{post.user_name}</S.UserName>
+            <S.CreateAt>{post.create_at}</S.CreateAt>
+            <S.Tag>{post.tag}</S.Tag>
+          </S.UserInfo>
+        </S.PostContent>
+        <S.PostImage src={post.postImage} alt={post.title} />
+      </S.PostCard>
+    ))}
+  </S.SearchContainer>
   );
 }
