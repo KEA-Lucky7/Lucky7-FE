6224de536a323d5d4ba65426186b088b57afcde5
jrary <jrary@naver.com>
2024-05-28 11:31:19 +0900

    feat: 로그인 redirect 설정

--- src/components/homePage/login/Login.tsx
+++ src/components/homePage/login/Login.tsx
@@ -1,23 +1,22 @@
 import * as S from "../styles/LoginStyle";
 import { useNavigate } from "react-router-dom";
 
-// { setShowLoginModal }: Props
 const Login = () => {
   const googleId = import.meta.env.VITE_GOOGLE_ID;
   const redirectUri = import.meta.env.VITE_SERVER_URL;
   const testUri = import.meta.env.VITE_FRONT_TEST_URL
-  // const kakaoKey = import.meta.env.VITE_KAKAO_KEY;
+  const kakaoKey = import.meta.env.VITE_KAKAO_KEY;
 
   // 구글 소셜로그인
   const handleGoogleLogin = () => {
-    const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${testUri}/login/redirect&response_type=code&client_id=${googleId}&scope=https://www.googleapis.com/auth/userinfo.profile`;
+    const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${testUri}/login/redire&response_type=code&client_id=${googleId}&scope=https://www.googleapis.com/auth/userinfo.profile`;
     window.location.href = GOOGLE_AUTH_URL;
   };
 
   // 카카오 소셜로그인
   const handleKakaoLogin = () => {
     const KAKAO_AUTH_URL = `${redirectUri}/oauth2/authorization/kakao`;
-    const KAKAO_AUTH_URL2 = `https://kauth.kakao.com/oauth/authorize`;
+    const KAKAO_AUTH_URL2 = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoKey}&redirect_uri=${testUri}/login/redirect&response_type=code`;
     window.location.href = KAKAO_AUTH_URL2;
   }
 
