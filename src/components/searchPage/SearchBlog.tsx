98ffe8fe6208a2522360209d97c7b2411a38e806
jrary <jrary@naver.com>
2024-05-30 14:52:49 +0900

    feat: 검색 페이징 진행 중 임시 저장용

--- src/components/searchPage/SearchBlog.tsx
+++ src/components/searchPage/SearchBlog.tsx
@@ -1,17 +1,56 @@
-import * as S from "./styles/SearchBlogStyle";
-import searchBlog from "../../data/search/searchBlogList.json";
+import React, { useEffect, useState } from 'react';
+import * as S from './styles/SearchBlogStyle';
+
+interface Blog {
+  blogId: number;
+  blogName: string;
+  blogAbout: string;
+  blogOwnerName: string;
+  blogOwnerImageUrl: string;
+}
 
 export default function SearchBlog() {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await fetch('https://api.example.com/blogs');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setBlogs(data.content);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
+  if (loading) {
+    return <S.Loading>Loading...</S.Loading>;
+  }
+
+  if (error) {
+    return <S.Error>Error: {error}</S.Error>;
+  }
+
   return (
     <S.SearchContainer>
-      {searchBlog.map((blog, index) => (
-        <S.BlogCard key={index}>
+      {blogs.map((blog) => (
+        <S.BlogCard key={blog.blogId}>
           <S.BlogContent>
-            <S.BlogTitle>{blog.title}</S.BlogTitle>
-            <S.BlogDescription>{blog.describe}</S.BlogDescription>
+            <S.BlogTitle>{blog.blogName}</S.BlogTitle>
+            <S.BlogDescription>{blog.blogAbout}</S.BlogDescription>
             <S.UserInfo>
-              <S.UserImage src={blog.user_image} alt={blog.user} />
-              <S.UserName>{blog.user}</S.UserName>
+              <S.UserImage src={blog.blogOwnerImageUrl} alt={blog.blogOwnerName} />
+              <S.UserName>{blog.blogOwnerName}</S.UserName>
             </S.UserInfo>
           </S.BlogContent>
         </S.BlogCard>
