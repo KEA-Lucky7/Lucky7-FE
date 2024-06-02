990224def4bc31b362fc958371ba100b6aa720d3
jrary <jrary@naver.com>
2024-06-02 23:20:50 +0900

    feat: zustand 토큰저장

--- src/components/homePage/login/LoginInfo.tsx
+++ src/components/homePage/login/LoginInfo.tsx
@@ -1,10 +1,13 @@
+import { useEffect } from "react";
 import * as S from "../styles/LoginInfoStyle";
 import { useNavigate } from "react-router-dom";
+import { useStoreJwt } from './state';
+
 
 const LoginInfo = () => {
-
-
   const navigate = useNavigate();
+  const { accessToken, refreshToken } = useStoreJwt();
+
   const hideLoginModal = () => {
     const hide = window.confirm("회원가입을 그만두시겠습니까? 입력한 정보가 삭제됩니다.");
     if (hide) {
@@ -12,28 +15,29 @@ const LoginInfo = () => {
     }
   };
 
+  useEffect(() => {
+    console.log(accessToken);
+    console.log(refreshToken);
+  }, [accessToken, refreshToken]);
+
   return (
     <S.LoginOverlay onClick={hideLoginModal}>
       <S.LoginContainer onClick={(e) => e.stopPropagation()}>
-        
         <S.LoginTitleContainer>모아보아를 시작하기 전에,<br />기본 정보를 입력해 주세요</S.LoginTitleContainer>
-        
         <S.LoginContents>
         닉네임
         <S.LoginInputContainer
           placeholder="닉네임"
           />
         </S.LoginContents>
-
         <S.LoginContents>
-        생년월일
+          생년월일
         <S.LoginInputContainer
           placeholder="0000/00/00"
           />
         </S.LoginContents>
-
         <S.LoginContents>
-        이메일
+          이메일
         <S.LoginInputContainer
           placeholder="example@example.com"
           />
--- src/components/homePage/login/LoginRedirect.tsx
+++ src/components/homePage/login/LoginRedirect.tsx
@@ -1,30 +1,26 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useStore } from "./state";
+import { useStoreJwt } from "./state";
 
 const LoginRedirect = () => {
   const navigate = useNavigate();
   const [isRegisteredIn, setIsRegisteredIn] = useState(false);
-  const { setUserToken } = useStore();
+  const { setUserToken } = useStoreJwt();
+
+  const query = new URLSearchParams(window.location.search);
+  const access_token = query.get("access_code");
+  const refresh_token = query.get("redirect_code");
 
   useEffect(() => {
-
-    const query = new URLSearchParams(location.search);
-    const token = query.get("code");
-
-    if (token) {
-        setUserToken(token);
-        setIsRegisteredIn(true)
+    if (access_token && refresh_token) {
+      window.alert(`${access_token}\n${refresh_token}`);
+      setUserToken(access_token, refresh_token);
+      setIsRegisteredIn(true);
+      navigate("/login/info");
+    } else {
+      window.alert("아직안옴.(과연아직일까?)");
     }
-
-  }, [setUserToken]);
-
-  useEffect(() => {
-    if (isRegisteredIn) {
-      
-      //navigate("/login/info");
-    }
-  }, [isRegisteredIn]);
+  }, [access_token, refresh_token, navigate, setUserToken]);
 
   return null;
 };
