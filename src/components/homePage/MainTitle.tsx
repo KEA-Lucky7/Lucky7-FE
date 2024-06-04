ed6c9f33d2d36904a96c32dd8e9f4bd98693e88e
HOAN_C <hoan.c9907@gmail.com>
2024-06-04 09:08:10 +0900

    style/메인 페이지 타이틀 스타일 변경

--- src/components/homePage/MainTitle.tsx
+++ src/components/homePage/MainTitle.tsx
@@ -1,14 +1,19 @@
 import * as S from "../homePage/styles/MainTitle";
-import BackGroundImageUrl from "../../assets/home/BackGroundImg.png";
+import mainImg from "../../assets/home/mainImg.png";
 
 const MainTitle = () => {
   return (
-    <S.MainTitleContainer imageUrl={BackGroundImageUrl}>
-      <S.TextContainer>
-        <S.Text>차곡차곡 모아봐요,</S.Text>
-        <S.Text>소비와 함께하는 즐거운 기록</S.Text>
-        <S.ByMoaBoa>by moaboa</S.ByMoaBoa>
-      </S.TextContainer>
+    <S.MainTitleContainer>
+      <img
+        src={mainImg}
+        style={{
+          scale: "60%",
+          position: "absolute",
+          right: "-80px" /* 우측 여백 */,
+          bottom: "-80px" /* 하단 여백 */,
+          // border: "1px solid red",
+        }}
+      />
     </S.MainTitleContainer>
   );
 };
