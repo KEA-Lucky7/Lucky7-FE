4f9d5cf936ffc2e68700c92a9849e1c6aa528428
HOAN_C <hoan.c9907@gmail.com>
2024-06-07 01:22:23 +0900

    레포트 디자인 및 더미데이터 삽입

--- src/components/reportPage/styles/ReportStyle.ts
+++ src/components/reportPage/styles/ReportStyle.ts
@@ -1,15 +1,15 @@
-// import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  border: 1px solid red;
+
+  // border: 1px solid red;
 `;
 
 export const ReportTitle = styled.div`
   font-size: 28px;
   margin: 10px 0px 0px 0px;
-  border: 1px solid red;
+  // border: 1px solid red;
 `;
 
 export const ReportContent = styled.div`
@@ -21,12 +21,41 @@ export const ReportCompare = styled.div`
   margin: 20px 0 20px 0;
 `;
 
-export const AIReport = styled.div`
-  font-size: 16px;
-  margin: 20px 0 20px 0;
-  padding: 12px;
+export const AIBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   // border: 1px solid red;
+`;
+
+export const AIReporContainer = styled.div`
+  margin: 0 auto;
+  width: 80%;
+  font-size: 16px;
+  margin: 25px 0 20px 0;
+  padding: 12px;
   border-radius: 8px;
   letter-spacing: 1px;
-  background: lightgray;
+  background: #ececec;
+  border: 1px solid #d4d4d4;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const AILogo = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background: #4566e3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
+
+export const AIContent = styled.div`
+  width: 90%;
+  // border: 1px solid red;
+  color: #393939;
 `;
